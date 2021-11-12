ee8fd0492607fab1120bbe6e64a830e2827eba43
GaneshN29 <77326035+GaneshN29@users.noreply.github.com>
2021-11-12 20:58:48 +0530

    Add files via upload

--- lab1-oop/Lab2Prg2.java
+++ lab1-oop/Lab2Prg2.java
@@ -5,19 +5,19 @@ import java.util.Scanner;
 public class Lab2Prg2 {
 
 	public static void main(String[] args) {
-		int a, b, c;
-		Scanner sc1 = new Scanner(System.in);
-		System.out.print("Enter a: ");
-		a = sc1.nextInt();
-		Scanner sc2 = new Scanner(System.in);
-		System.out.print("Enter b: ");
-		b = sc2.nextInt();
-		Scanner sc3 = new Scanner(System.in);
-		System.out.print("Enter c: ");
-		c = sc3.nextInt();
-		
-		int ans = (c > ((a>b)? a:b))? c : (a>b)? a:b;
-		System.out.println(ans);
+	
+		public static void main(final String[] args) {
+			final Scanner Sc = new Scanner(System.in);
+			System.out.print("Enter the number a,b and c:");
+			final int a = Sc.nextInt();
+			final int b = Sc.nextInt();
+			final int c = Sc.nextInt();
+			int temp = (a > b) ? a : b;
+			int larg = (temp > c) ? temp : c;
+			System.out.println("Largest No:" + larg);
+			temp = ((a < b) ? a : b);
+			larg = ((temp < c) ? temp : c);
+			System.out.println("Smallest No:" + larg);
 	}
 
 }
